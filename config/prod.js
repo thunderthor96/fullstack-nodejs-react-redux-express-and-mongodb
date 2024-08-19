274e7ad0288b74e2ce8ea0145858276051bbb251
Bobby B <bloopsocket@gmail.com>
2024-08-19 16:08:30 -0500

    domain update

--- config/prod.js
+++ config/prod.js
@@ -8,4 +8,5 @@ module.exports = {
   stripeSecretKey: process.env.STRIPE_SECRET_KEY,
   mailgunKey: process.env.MAILGUN_KEY,
   mailgunDomain: process.env.MAILGUN_DOMAIN,
+  redirectDomain: process.env.REDIRECT_DOMAIN,
 };
